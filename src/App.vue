7ad61b36aabc0fe0e6bfb8629d39cae3c0f3b5f3
andrew <andrewmuscara@gmail.com>
2021-01-16 20:51:12 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <WishList />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import WishList from "./components/WishList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    WishList,
+  },
 };
 </script>
 
---
+++ src/components/WishList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>My Wishlist boi !!!!!!!!</h1>
+    <input type="text" v-model="filterText" />
+
+    <ul :class="$style.wishlist">
+      <li :key="gift.id" v-for="gift in filteredWithList">
+        <h2>{{ gift.label }}</h2>
+        <img :src="`/images/${gift.image}`" :alt="gift.label" />
+        <p>{{ gift.votes }}</p>
+        <button>wish for this</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gifts from "../../public/data.json";
+export default {
+  data: () => ({
+    gifts,
+    filterText: "",
+  }),
+  computed: {
+    filteredWithList: function() {
+      return this.gifts.filter((gift) => {
+        const label = gift.label.toLowerCase();
+        const search = this.filterText.toLowerCase();
+        return label.includes(search);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.wishlist {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, 150px);
+  grid-template-rows: auto;
+  list-style: none;
+  padding: 0;
+  justify-content: center;
+
+  h2 {
+    font-size: 1rem;
+  }
+
+  img {
+    width: 100%;
+  }
+}
+</style>
